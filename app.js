b0d75f51a2d15ee4117dadca3f408b5fd4b9047d
noclipping <aresous@gmail.com>
2023-12-18 22:33:40 -0500

    fix

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const { Client } = require('pg');
 const { Strategy, ExtractJwt } = require('passport-jwt');
+require('dotenv').config()
 
+console.log(process.env.DB_NAME, process.env.DB_PASSWORD) 
 
 const app = express()
 const port = 3000
@@ -14,7 +16,7 @@ const port = 3000
 const client = new Client({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    database: 'zenvibe_DB',
+    database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
   });
@@ -29,7 +31,7 @@ passport.use(new LocalStrategy(
             }
 
             const user = result.rows[0];
-            if(!user || !bcrypt.compareSync(password, user.password)) {
+            if(!user || !bcrypt.compareSync(password, user.password_hash)) {
                 return done(null, false)
             }
             return done(null, user);
@@ -38,26 +40,31 @@ passport.use(new LocalStrategy(
 ))
 
 passport.use('jwt', new Strategy(
-    {
-        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-        secretOrKey: process.env.JWT_SECRET
-    },
-    (jwtPayload, done)=>{
-        client.query('SELECT * FROM users where id = $1', [jwtPayload.sub], (err, result)=>{
-            if(err){
-                return done(err,false)
-            }
+  {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET
+  },
+  (jwtPayload, done) => {
+    // Log the extracted token
+    const token = ExtractJwt.fromAuthHeaderAsBearerToken();
+    console.log('Extracted Token:', token);
 
-            const user = result.rows[0]
+    console.log('JWT Payload:', jwtPayload); // Log the payload
+    client.query('SELECT * FROM users where user_id = $1', [jwtPayload.sub.user_id], (err, result) => {
+      if (err) {
+        return done(err, false);
+      }
 
-            if(user){
-                return done(null, user)
-            } else {
-                return done(null,false)
-            }
-        })
-    }
-))
+      const user = result.rows[0];
+
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  }
+));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -65,13 +72,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 
 app.post('/register', (req, res) => {
-  const token = jwt.sign({ sub: userId }, process.env.JWT_SECRET);
-  res.json({ token });
+  // const token = jwt.sign({ sub: req.userId }, process.env.JWT_SECRET);
+  // res.json({ token });
+  console.log(req.body.username,req.body.password)
+  let username = req.body.username;
+  let password = req.body.password;
   
     // Check if username is already taken
     client.query('SELECT * FROM users WHERE username = $1', [username], (err, result) => {
       if (err) {
-        return res.status(500).json({ error: 'Internal Server Error' });
+        return res.status(500).json({ error: err });
       }
   
       if (result.rows.length > 0) {
@@ -81,31 +91,45 @@ app.post('/register', (req, res) => {
       // If username is available, hash the password and create the user
       const hashedPassword = bcrypt.hashSync(password, 10);
   
-      client.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id',
+      client.query('INSERT INTO users (username, password_hash) VALUES ($1, $2) RETURNING user_id',
         [username, hashedPassword], (err, result) => {
           if (err) {
+            console.log(err,'err')
             return res.status(500).json({ error: 'Internal Server Error' });
           }
   
-          const userId = result.rows[0].id;
-          const token = jwt.sign({ sub: userId }, process.env.JWT_SECRET);
+          const user = result.rows[0]
+          const token = jwt.sign({ sub: user }, process.env.JWT_SECRET);
           res.json({ token });
         });
     });
   });
   
 
-app.post('/login', passport.authenticate('local', {session: false}), (req,res)=>{
-    const token = jwt.sign({sub:req.user.id}, process.env.JWT_SECRET)
+app.post('/login', passport.authenticate('local', {session: false}), (req,res)=>{  
+    const token = jwt.sign({sub:req.user}, process.env.JWT_SECRET)
     res.json({ token });
 })
 
-app.get('/profile', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const user = req.user;
-    res.send(`Hello ${user.username}, welcome home!`)
-  });
+app.get('/profile', (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) {
+      console.log('Error:', err);
+      return res.status(500).json({ error: err });
+    }
+    if (!user) {
+      console.log('Authentication Failed. Info:', info);
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    req.user = user; // Manually assign the user to the request object
+    next(); // Proceed to the next middleware
+  })(req, res, next);
+}, (req, res) => {
+  // Your original route logic
+  console.log(req.user);
+  res.send(`Hello ${req.user.username}, welcome home!`);
+});
   
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
