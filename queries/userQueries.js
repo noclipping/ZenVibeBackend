bcd4c1f87b741b70104bcf87a09847d64776e15a
eajasin <elizabethajasin@gmail.com>
2023-12-23 12:31:42 -0500

    userQueries

--- queries/userQueries.js
+++ queries/userQueries.js
@@ -17,31 +17,29 @@ const deleteUser = (req, res) => {
     })
 }
 
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
     const userId = parseInt(req.params.id)
-    let { username, password_hash, email, original_weight, height, age, goal_weight } = req.params
-    const preExistingData = db.pool.query(`SELECT * FROM users WHERE user_id = $1`, [userId])
-    if (!username || username.length <= 0) {
-        username = preExistingData.rows[0].username
-    }
-    if (!password_hash || password_hash.length <= 0){
-        password_hash = preExistingData.rows[0].password_hash
-    }
-    if (!email || email.length <= 0){
-        email = preExistingData.rows[0].email
-    }
-    if(!original_weight || original_weight.length <= 0){
-        original_weight = preExistingData.rows[0].original_weight
-    }
-    if (!height || height.length <= 0){
-        height = preExistingData.rows[0].height
-    }
-    if (!age || age.length <= 0){
-        age = preExistingData.rows[0].age
-    }
-    if (!goal_weight || goal_weight <= 0){
-        goal_weight = preExistingData.rows[0].goal_weight
-    }
+    console.log(req.params, "req.params")
+    const { username, password_hash, email, original_weight, height, age, goal_weight } = req.body
+    const preExistingData = await db.pool.query(`SELECT * FROM users WHERE user_id = $1`, [req.params.id])
+    
+    const updatedUsername = username || preExistingData.rows[0].username;
+    const updatedPasswordHash = password_hash || preExistingData.rows[0].password_hash;
+    const updatedEmail = email || preExistingData.rows[0].email;
+    const updatedOriginalWeight = Number(original_weight) || preExistingData.rows[0].original_weight;
+    const updatedHeight = Number(height) || preExistingData.rows[0].height;
+    const updatedAge = Number(age) || preExistingData.rows[0].age;
+    const updatedGoalWeight = Number(goal_weight) || preExistingData.rows[0].goal_weight;
+
+
+const result = await db.pool.query(
+   `UPDATE users SET username = $1, password_hash = $2, email = $3, original_weight = $4, height = $5, age = $6, goal_weight = $7 WHERE user_id = ${req.params.id} RETURNING *`,
+   [updatedUsername, updatedPasswordHash, updatedEmail, updatedOriginalWeight, updatedHeight, updatedAge, updatedGoalWeight]
+    // `UPDATE users SET email = $1, original_weight = $2, height = $3, age = $4, goal_weight = $5 WHERE user_id = ${req.params.id} RETURNING *`,
+    // [updatedEmail, updatedOriginalWeight, updatedHeight, updatedAge, updatedGoalWeight]
+
+    )
+    res.json(result.rows[0])
 
 }
 
