60ae9856082b701a960708107fa64f2317e2654c
Jmenzies722 <Jmenzies722@gmail.com>
2024-01-31 21:12:37 -0500

    update port

--- app.js
+++ app.js
@@ -488,8 +488,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on port ${port}`);
 });
 
 exports.client = client;
